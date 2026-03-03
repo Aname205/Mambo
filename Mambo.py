c7810e1daf8db199b80d590b06418b0aa4d220d6
AnhTuan12941 <littlehebj@gmail.com>
2026-03-04 02:39:53 +0700

    Balance

--- Mambo.py
+++ Mambo.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 import logging
 from dotenv import load_dotenv
+import asyncio
+import aiosqlite
 import os
 from keep_alive import keep_alive
 
@@ -13,20 +15,54 @@ keep_alive()
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 intents = discord.Intents.default()
 intents.members = True
-intents.messages = True
+intents.message_content = True
 
 bot = commands.Bot(command_prefix='m', intents=intents)
 
 @bot.event
 async def on_ready():
     print("Mambo")
+    bot.db = await aiosqlite.connect("MyBank.db")
+    await asyncio.sleep(5)
+    async with bot.db.cursor() as cursor:
+        await cursor.execute("""CREATE TABLE IF NOT EXISTS Mambo(
+            wallet INTEGER,
+            bank INTEGER,
+            user INTEGER)"""
+        )
+    await bot.db.commit()
+    print("Database created")
+
+async def create_balance(user):
+    async with bot.db.cursor() as cursor:
+        await cursor.execute("""INSERT INTO Mambo(wallet, bank, user) VALUES(?, ?, ?)""", (100, 0, user.id))
+    await bot.db.commit()
+
+async def get_balance(user):
+    async with bot.db.cursor() as cursor:
+        await cursor.execute("""SELECT wallet, bank FROM Mambo WHERE user = ?""", (user.id,))
+        data = await cursor.fetchone()
+
+    if data is None:
+        await create_balance(user)
+        return 100, 0
+
+    wallet, bank = data
+    return wallet, bank
+
+@bot.command()
+async def balance(ctx):
+    wallet, bank = await get_balance(ctx.author)
+    em = discord.Embed(
+        title=f"{ctx.author.name}'s Balance",
+        color=discord.Color.blue(),
+    )
+    em.add_field(name="Wallet", value=wallet)
+    em.add_field(name="Bank", value=bank)
+    await ctx.send(embed=em)
 
 @bot.command()
 async def mambo(ctx):
     await ctx.send(f"{ctx.author.mention} Mambo")
-
-@bot.command()
-async def anngu(ctx):
-    await ctx.send("Tung An pha gia chi tu")
 
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
