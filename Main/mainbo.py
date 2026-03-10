e52c4ae181a69304540ab3b8d40b4652204a6411
VuLL-Git <blockfull34@gmail.com>
2026-03-10 17:15:54 +0700

    add poker

--- Main/mainbo.py
+++ Main/mainbo.py
@@ -39,6 +39,7 @@ async def main():
         await bot.load_extension("Commands.Currency.highlow")
         await bot.load_extension("Commands.Currency.rps")
         await bot.load_extension("Commands.Currency.slot")
+        await bot.load_extension("Commands.Currency.poker")
         await bot.start(token)
 
 asyncio.run(main())
